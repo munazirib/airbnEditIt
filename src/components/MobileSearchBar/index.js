c427aaa16e56e2e7b57067f43fc4e625e71d035e
munazirib Bienaime <munaziriBNM@gmail.com>
2023-10-05 14:48:07 +0000

    nn

--- src/components/MobileSearchBar/index.js
+++ src/components/MobileSearchBar/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 
 function MobileSearchBar() {
   return <div className="mobile-search-bar">
-       <input id="search" type="text" name="search" placeholder="search products and services"/>
+       <input id="search" type="text" name="search" placeholder=" Search Here"/>
        <span><i className="fa-solid fa-magnifying-glass search-icon" style={{color:"#ffffff"}}></i></span>
       </div>;
 }
